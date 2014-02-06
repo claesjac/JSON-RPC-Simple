6e936f70c6ccb5801ccb9d98099f6542a4b01fd4
Claes Jakobsson <claes@surfar.nu>
2014-02-06 09:45:58 +0100

    Bumped to version 0.06

--- lib/JSON/RPC/Simple.pm
+++ lib/JSON/RPC/Simple.pm
@@ -6,7 +6,7 @@ use warnings;
 use Scalar::Util qw(blessed refaddr);
 use Carp qw(croak);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 our $ClientClass = "JSON::RPC::Simple::Client";
 sub connect {
